d4c90da60432a4a27523b44fb7d6adcf33f319c5
Rahdeg <walett95@gmail.com>
2023-08-22 12:54:24 +0100

    s

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 
+import Link from "next/link";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import Music from "./components/Music";
@@ -8,6 +9,7 @@ import "./globals.css";
 export default function Home() {
   return (
     <main className=" bg-[#141A1F]">
+      <Link href="https://fonts.googleapis.com/css2?family=Red+Rose:wght@300;400;500;600;700&family=Lato:wght@100;300;400;700&display=swap"/>
       <Header />
       <Menu/>
       <Music/>
